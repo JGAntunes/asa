9e6ae61ed44eeb5ecaf6d570b3641bf152070219
JGAntunes <j.goncalo.antunes@gmail.com>
2014-04-18 21:55:58 +0100

    main + connect (unilateral)

--- asa.c
+++ asa.c
@@ -4,6 +4,7 @@
 typedef struct nodeAdj{
     int _id;
     int _flow;
+    int _cap;
     struct nodeAdj * _next;
 }* adj;
 
@@ -19,11 +20,12 @@ int maxFlow;
 int USER_NUM;
 int CN_NUM;
 int CRIT_NUM;
+int INF = -1;
 
 void initGraph(){
   int i;
   graph = (node *) malloc (USER_NUM * sizeof(node));
-  for(i = 0; i < USER_NUM; i++){
+  for(i = 0; i < USER_NUM + 2; i++){
     graph[i] = (node) malloc (sizeof(struct node_s));
     graph[i]->_user = (user) malloc (sizeof( struct user_s));
     graph[i]->_id = i;
@@ -32,12 +34,34 @@ void initGraph(){
   }
 }
 
-void connect(int connectingNode, int nodeToConnect){
+void EdmondsKarp(){
+    output:
+        f             (Value of maximum flow)
+        F             (A matrix giving a legal flow with the maximum value)
+    f := 0 (Initial flow is zero)
+    F := array(1..n, 1..n) (Residual capacity from u to v is C[u,v] - F[u,v])
+    forever
+        m, P := BreadthFirstSearch(C, E, s, t, F)
+        if m = 0
+            break
+        f := f + m
+        (Backtrack search, and write flow)
+        v := t
+        while v ≠ s
+            u := P[v]
+            F[u,v] := F[u,v] + m
+            F[v,u] := F[v,u] - m
+            v := u
+    return (f, F)
+}
+
+void connect(int connectingNode, int nodeToConnect, int flow, int cap){
   node graphNode = graph[connectingNode];
   adj newNode = (adj) malloc (sizeof(struct nodeAdj));
   newNode->_next = NULL;
-  newNode->_id = nodeToShare;
-  newNode->flow = 0:
+  newNode->_id = nodeToConnect;
+  newNode->_flow = flow:
+  newNode->_cap = cap;
   if(!(graphNode->_adjEnd)){
     graphNode->_adj = newNode;
     graphNode->_adjEnd = newNode;
@@ -48,22 +72,29 @@ void connect(int connectingNode, int nodeToConnect){
   }
 }
 
-
-
 int main(){
   scanf("%d", &USER_NUM);
-  scanf("%d", &SHARE_NUM);
-  int user, link;
+  scanf("%d", &CN_NUM);
+  int user, link, crit_sit, crit_node;
   initGraph();
-  int i = 0;
-  while(i++ < SHARE_NUM){
+  int i = 0, j = 0;
+  while(i++ < CN_NUM){
     /*printf("le utilizador %d\n", i); */
     scanf("%d", &user);
     /*printf("le link %d\n", i); */
     scanf("%d", &link);
-    connect(user-1, link-1);
+    connect(user+1, link+1, 1, 0);
   }
-  
-  
+  scanf("%d", &CRIT_NUM);
+  i = 0;
+  while(i++ < CRIT_NUM){
+    scanf("%d", &crit_sit);
+    j = 0;
+    while(j++ < crit_sit){
+      scanf("%d", &crit_node);
+      connect(0, crit_node+1, INF, INF);
+      connect(1, crit_node+1, INF, INF);
+    }
+  }
   return 0;
 }
